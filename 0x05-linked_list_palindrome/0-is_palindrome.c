a108e7f4e852358657e2df373dd3b96e56c338d6
Giovanni Perez <op7puro@live.com>
2021-05-26 01:17:21 -0500

    Fix betty

--- 0x05-linked_list_palindrome/0-is_palindrome.c
+++ 0x05-linked_list_palindrome/0-is_palindrome.c
@@ -9,36 +9,41 @@
  */
 int is_palindrome(listint_t **head)
 {
-  listint_t *start;
-  listint_t *end;
-  listint_t *tmp = *head;
-  int count = 0;
+	listint_t *start;
+	listint_t *end;
+	listint_t *tmp = *head;
+	int count = 0;
 
-  while (tmp != NULL)
-  {
-    count++;
-    tmp = tmp->next;
-  }
+	if (*head == NULL)
+	{
+		return 1;
+	}
 
-  for (int i = 0; i < count / 2; i++)
-  {
-    end = *head;
-    start = *head;
+	while (tmp != NULL)
+	{
+		count++;
+		tmp = tmp->next;
+	}
 
-    for (int k = 0; k < i; k++)
-    {
-      start = start->next;
-    }
+	for (int i = 0; i < count / 2; i++)
+	{
+		end = *head;
+		start = *head;
 
-    for (int j = (count - 1) - i; j > 0; j--)
-    {
-      end = end->next;
-    }
+		for (int k = 0; k < i; k++)
+		{
+			start = start->next;
+		}
 
-    if (start->n != end->n)
-    {
-      return 0;
-    }
-  }
-  return 1;
+		for (int j = (count - 1) - i; j > 0; j--)
+		{
+			end = end->next;
+		}
+
+		if (start->n != end->n)
+		{
+			return 0;
+		}
+	}
+	return 1;
 }
